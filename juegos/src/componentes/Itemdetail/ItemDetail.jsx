f89b333311db0159dbd6246c8284b4c23e371ebb
apanauar <apaferreira28@gmail.com>
2024-09-23 17:38:02 -0300

    css

--- juegos/src/componentes/Itemdetail/ItemDetail.jsx
+++ juegos/src/componentes/Itemdetail/ItemDetail.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ItemDetail = ({id,nombre,precio,img}) => {
+const ItemDetail = ({id,nombre,precio,img,description}) => {
   return (
     <div>
       <h2>Nombre: {nombre}</h2>
       <h3>Precio {precio}</h3>
       <h3>ID:{id}</h3>
       <img src={img} alt={nombre} />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus nemo quasi, eaque aspernatur culpa eligendi nobis, earum iure commodi rem totam porro ipsum, vitae sint eius. Dolorem, aut ipsa.</p>
+      <p>Descripcion:{description}</p>
     </div>
   )
 }
